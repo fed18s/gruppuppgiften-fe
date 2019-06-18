5b965ade544acc72157d33504f5f918b38536053
Viktorija Valsoe <johndoe@example.com>
2019-06-18 10:00:57 +0200

    Added input field and regex expression, parmis johanna viktorija

--- src/js/animalApp.js
+++ src/js/animalApp.js
@@ -91,10 +91,66 @@
     })
   }
 
+  const $form1 = document.getElementById('form-1');
+  const $form2 = document.getElementById('form-2');
+  const tagRegex = /<.*>/;
+
+  function onSubmitForm(e) {
+    let formValid = true;
+    console.log('hello inside onsubmit, outside loop');
+    for (let i = 0; i<e.target.length; i++) {
+      e.target[i].style.borderColor = '';
+      const inputValue = e.target[i].value;
+      const inputDataType = e.target[i].getAttribute('data-type');
+      console.log(inputValue.match(tagRegex));
+      console.log('hello inside loop');
+
+      switch (inputDataType) {
+        case 'username':
+        case 'birthdate':
+          if (inputValue.match(tagRegex)) {
+            alert("Don't hack, don't hack me no more");
+            e.target[i].className = 'error';
+            formValid = false;
+          }
+          else {
+            e.target[i].className = '';
+          }
+          break;
+        }
+    }
+
+    // If form is not valid, we prevent default action - which is submitting form
+    //if (!formValid) {
+      //e.preventDefault();
+    //}
+  }
+
+  function onInvalid(e) {
+    e.preventDefault();
+    //console.log(e.target.getAttribute('id'));
+    e.target.style.borderColor = 'red';
+  }
+
+  function registerInvalidListeners(form) {
+    for (let i=0; i<form.length; i++) {
+      form[i].addEventListener('invalid', onInvalid);
+    }
+  }
+
+  function registerListeners() {
+    $form1.addEventListener('submit', onSubmitForm);
+    registerInvalidListeners($form1);
+    $form2.addEventListener('submit', onSubmitForm);
+    registerInvalidListeners($form2);
+
+  }
+
   function pageLoaded() {
     populateSelect(animalType);
     listenToSelect();
     listenToAdd();
+    registerListeners();
   }
 
   window.pageLoaded = pageLoaded;
